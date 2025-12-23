d5b23822167064414f52a92343968dea1362c2b5
Farzanayesmin21 <yesmin21212@gmail.com>
2025-12-23 18:19:18 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Current Date & Time
+const now = new Date();
+document.getElementById("dateTime").innerHTML =
+  now.toDateString() + " " + now.toLocaleTimeString();
+
+
+  function signupConfirm() {
+  alert("Signup Successful!");
+}
+
+function loginConfirm() {
+  alert("Login Successful!");
+}
+
